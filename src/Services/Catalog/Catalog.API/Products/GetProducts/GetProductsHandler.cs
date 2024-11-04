d2986bacaf7db80b10998d6cd34d1a29d12faaf6
Roman Shahrear <roman.shahrear@gmail.com>
2024-11-04 22:02:21 +0600

    Add Get product

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    internal class GetProductsQueryHandler
+        (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        : IQueryHandler<GetProductsQery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            
+            return new GetProductsResult(products);
+        }
+    }
+}
