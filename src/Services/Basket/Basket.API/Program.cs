bafd9b0f8af2479e09b134521acd0bb45b7a6eb5
Roman Shahrear <roman.shahrear@gmail.com>
2024-11-10 22:31:59 +0600

    Add Basket feature without database

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,8 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 
+//Application Services
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
+
+builder.Services.AddCarter();
+
 var app = builder.Build();
 
 // configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
