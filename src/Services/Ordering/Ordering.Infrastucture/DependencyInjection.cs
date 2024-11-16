3e0b971c3f65b5bff933efe18c2b89886758b706
Roman Shahrear <roman.shahrear@gmail.com>
2024-11-16 14:25:18 +0600

    Adding layer architecture and also Add Ordering.API in DI by every single DI

---
+++ src/Services/Ordering/Ordering.Infrastucture/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastucture
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectingString = configuration.GetConnectionString("Database");
+
+            // Add Services to the container
+            // Services.AddDbContext<ApplicationDbContext>(options => 
+            //      options.UseSqlServer(connectionString);
+
+            // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
